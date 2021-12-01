aff368bf6a9c47ea3f5107b29e7d71e8b381a197
Myrtraz <marcoestaba39@gmail.com>
2021-12-01 17:16:02 -0500

    Rework del proyecto Fuego Quasar

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -13,25 +13,64 @@ class GeneralController extends Controller
     public function topsecret(Request $request)
     {
         $input = $request->all();
-        $d = array();
-        $p = array(
-            array($input['satellites'][0]['message']),
-            array($input['satellites'][1]['message']),
-            array($input['satellites'][2]['message'])
-        );
+        
+        if (! empty($input['satellites'][0]) && ! empty($input['satellites'][1]) && ! empty($input['satellites'][2]) == true) {
+          $array = array(
+            $input['satellites'][0]['message'],
+            $input['satellites'][1]['message'],
+            $input['satellites'][2]['message']
+          );
 
-        $count = count($p);
-        for ($i= 0; $i<$count; $i++) {
-
-            for ($o= 0; $o < count(array($i)) ; $o++) {
-                $d[] = $p[$i][$o];
-
-
+          foreach(array_map('array_filter', $array) as $item) {
+            foreach($item as $key => $value) {
+              $message[] = $value;
             }
+          }
+
+          $sphere1 = new Sphere(-500.0, -200.0, $input['satellites'][0]['distance']);
+          $sphere2 = new Sphere(100.0, -100.0, $input['satellites'][1]['distance']);
+          $sphere3 = new Sphere(500.0, 100.0, $input['satellites'][2]['distance']);
+  
+          $trilateration = new Intersection($sphere1, $sphere2, $sphere3);
+          $point = $trilateration->position();
+
+          print_r($point);
+          return response()->json($message);
+
+        } else {
+
+          return abort(404, "Este satelite no existe o ha sido destruido");
         }
+      
+    }
 
-        dd($d[0][3]);
+    public function topsecret_split_name(Request $request)
+    {
+      $input = $request->all();
+      $array = array(
+        'name' =>  request()->satellite_name,
+        'distance' => $input['distance'],
+        'message' => $input['message']
+      );
 
+      return response()->json($array);
+    }
+
+    public function topsecret_split(Request $request)
+    {
+      $input = $request->all();
+      $array = array(
+        $input['satellites'][0]['message'],
+        $input['satellites'][1]['message'],
+        $input['satellites'][2]['message']
+      );
+      
+      foreach(array_map('array_filter', $array) as $item) {
+        foreach($item as $key => $value) {
+          $message[] = $value;
+        }
+      }
+      
         $sphere1 = new Sphere(-500.0, -200.0, $input['satellites'][0]['distance']);
         $sphere2 = new Sphere(100.0, -100.0, $input['satellites'][1]['distance']);
         $sphere3 = new Sphere(500.0, 100.0, $input['satellites'][2]['distance']);
@@ -39,71 +78,7 @@ class GeneralController extends Controller
         $trilateration = new Intersection($sphere1, $sphere2, $sphere3);
         $point = $trilateration->position();
 
-        dd($point);
-
-        return response()->json($point, $d);
-    }
-
-    public function topsecret_split_name(Request $request)
-    {
-      $input = $request->all();
-
-      request()->satellite_name;
-
-      $satellites = Satellites::create([
-        'name' =>  request()->satellite_name,
-        'distance' =>  $input['distance'],
-        'message' =>  $input['message'],
-      ]);
-
-      return response()->json($satellites);
-    }
-
-    public function topsecret_split(Request $request)
-    {
-      $id = 5;
-      $id1 = 15;
-      $id2 = 25;
-
-      $satellite = Satellites::where('id', $id)->first();
-      $satellite1 = Satellites::where('id', $id1)->first();
-      $satellite2 = Satellites::where('id', $id2)->first();
-
-        $arr= [$satellite->message];
-        $arr1= [$satellite1->message];
-        $arr2= [$satellite2->message];
-
-        $list = array_merge($arr, $arr1, $arr2);
-
-        $remove[] = "'";
-        $remove[] = '"';
-        $remove[] = "-";
-        $remove[] = ",";
-        $remove[] = " ";
-        $FileName = str_replace( $remove, "",  $list);
-
-        $z =  implode(" ", $FileName);
-
-        dd($z);
-      $sphere1 = new Sphere(-500.0, -200.0, $satellite->distance);
-      $sphere2 = new Sphere(100.0, -100.0, $satellite1->distance);
-      $sphere3 = new Sphere(500.0, 100.0, $satellite2->distance);
-
-      $trilateration = new Intersection($sphere1, $sphere2, $sphere3);
-      $point = $trilateration->position();
-
       print_r($point);
-
+      return response()->json($message);
     }
 }
-
-/*
-http://localhost:8000/api/topsecret?name[]=kenobi&distance[]=150
-&message[]="este", "", "un", ""&name[]=skywalker&distance[]=200
-&message[]="", "es", "un", ""&name[]=sato&distance[]=100
-&message[]="", "es", "", "mensaje"
-
-http://localhost:8000/api/topsecret_split/{nombre}
-
-http://localhost:8000/api/topsecret_split
-*/
